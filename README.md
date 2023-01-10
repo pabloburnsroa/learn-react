829cd3653d506c6263bf84aa349d6b63cd4bdb5b
Pablo Burns-Roa <pabloburnsroa@googlemail.com>
2023-01-10 13:41:39 +0000

    updates...again

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# upgraded-potato
 # learn-react
---
+++ learn-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LearnUseMemo from './learnUseMemo';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Lets practice our React skills!</header>
+      <LearnUseMemo />
+    </div>
+  );
+};
+
+export default App;
